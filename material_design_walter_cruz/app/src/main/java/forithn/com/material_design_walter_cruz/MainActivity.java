7892b3bd1c1bda22723c37ba932f24ff4f9b1490
Walter Alcides Cruz Alvarado <walteraca97@gmail.com>
2020-10-25 16:46:50 -0600

    Material Design
    Material Design

---
+++ material_design_walter_cruz/app/src/main/java/forithn/com/material_design_walter_cruz/MainActivity.java
@@ -0,0 +1,14 @@
+package forithn.com.material_design_walter_cruz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
